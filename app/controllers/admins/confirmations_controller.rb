360bfbd82139ea40a5898a37f47d046add8b8cdb
篠崎裕太 <shnzk@ShinozMacBook.local>
2020-10-05 22:36:28 +0900

    [Fix]deviceでadmins,usersのコントローラを修正

--- app/controllers/admins/confirmations_controller.rb
+++ app/controllers/admins/confirmations_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Users::Users::ConfirmationsController < Devise::ConfirmationsController
+class Admins::ConfirmationsController < Devise::ConfirmationsController
   # GET /resource/confirmation/new
   # def new
   #   super
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  
   root 'homes#top'
   get 'homes/about'
+
+  # ------------------------ユーザ側------------------------
+  scope module: :users do
+    
+  end
+  devise_for :users
+
+  # ------------------------管理者側------------------------
+  namespace :admins do
+    
+  end
+
+  scope module: :users do
+    devise_for :admins
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
