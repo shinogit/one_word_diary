67e60a477ef66b1566325a1eec2073fe28e284cc
篠崎裕太 <shnzk@ShinozMacBook.local>
2020-10-09 13:01:35 +0900

    [Create]scriptフォルダ作成（コマンドで初期データ投入）

---
+++ script/create_users.rb
@@ -0,0 +1,9 @@
+# $ bin/rails runner script/create_users.rb   をnginxサーバ内で実行することで初期データを投入できる
+
+# 初期ユーザー
+User.create!(email: 'a@a', password: 'aaaaaa', name: 'テスト名前a', nick_name: 'てすたーA', introduction: 'テスト名前aです。ニックネームはてすたーAです。4649!', is_deleted: false)
+User.create!(email: 'b@b', password: 'bbbbbb', name: 'テスト名前bb', nick_name: 'てすたーBB', introduction: 'テスト名前bbです。ニックネームはてすたーBBです。4645?!', is_deleted: false)
+User.create!(email: 'c@c', password: 'cccccc', name: 'テスト名前ccc', nick_name: 'てすたーCCC', introduction: 'テスト名前cccです。ニックネームはてすたーCCCです。4645!?!', is_deleted: false)
+
+# 初期管理者
+Admin.create!(email: 'z@z', password: 'zzzzzz', name: '管理者ZZZZ')
