dd2f610e2d722b8f291cdff35c3c382191865554
篠崎裕太 <shnzk@ShinozMacBook.local>
2020-10-11 21:26:56 +0900

    [fix]

--- db/migrate/20201005134842_create_comments.rb
+++ db/migrate/20201005134842_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
       t.integer :user_id
       t.integer :word_id
-      t.text :body,        null: false, default: ""
+      t.text :body,        null: false
 
       t.timestamps
     end
